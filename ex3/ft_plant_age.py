450cca64c20ece401fb38747f65fd559e2e7a05b
out-0 <ayoubarid000@gmail.com>
2025-12-17 21:26:01 +0100

    remove the function call

--- ex3/ft_plant_age.py
+++ ex3/ft_plant_age.py
@@ -1,9 +1,6 @@
 def ft_plant_age():
     plant_age: int = int(input("Enter plant age in days: "))
-    if (plant_age > 60):
+    if plant_age > 60:
         print("Plant is ready to harvest!")
     else:
         print("Plant needs more time to grow.")
-
-
-ft_plant_age()
