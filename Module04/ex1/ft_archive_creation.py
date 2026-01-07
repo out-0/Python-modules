4f14673dfaa53897f21aca592b0634a6469a40ae
out-0 <ayoubarid000@gmail.com>
2026-01-07 22:26:13 +0100

    Done

--- Module04/ex1/ft_archive_creation.py
+++ Module04/ex1/ft_archive_creation.py
@@ -12,9 +12,9 @@ def test_archive_creation() -> None:
     print("Inscribing preservation data...")
 
     entries = [
-        "{[}ENTRY 001{]} New quantum algorithm discovered",
-        "{[}ENTRY 002{]} Efficiency increased by 347%",
-        "{[}ENTRY 003{]} Archived by Data Archivist trainee"
+        "[ENTRY 001] New quantum algorithm discovered",
+        "[ENTRY 002] Efficiency increased by 347%",
+        "[ENTRY 003] Archived by Data Archivist trainee"
     ]
     # Process the lines
     for entry in entries:
