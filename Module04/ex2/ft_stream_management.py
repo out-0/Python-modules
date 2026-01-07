2c2039a51e85b79391d30d709f420ab6a198e623
out-0 <ayoubarid000@gmail.com>
2026-01-07 03:40:30 +0100

    Done testing the standard streamings with ex2

---
+++ Module04/ex2/ft_stream_management.py
@@ -0,0 +1,35 @@
+from sys import stdin, stderr, stdout
+
+
+def test_stream_management() -> None:
+    """Test the 3 standard files for streaming data"""
+
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
+    print("")
+
+    # Dimonstraite input buffer
+    # Removing newline stop the auto flashing
+    print("Input Stream active. Enter archivist ID: ", end="")
+    # Flashing stdout buffer manually
+    stdout.flush()
+    # Removing the new line at the end for clean printing later.list
+    # since readline read also the [enter] new line.
+    archivist_id = stdin.readline()[:-1]
+
+    print("Input Stream active. Enter status report: ", end="")
+    stdout.flush()
+    status_report = stdin.readline()[:-1]
+
+    print("")
+
+    stdout.write(f"[STANDARD] Archive status from "
+                 f"{archivist_id}: {status_report}\n")
+    stderr.write("[ALERT] System diagnostic: "
+                 "Communication channels verified\n")
+    stdout.write("[STANDARD] Data transmission complete\n")
+
+    print("")
+    print("Three-channel communication test successful.")
+
+
+test_stream_management()
