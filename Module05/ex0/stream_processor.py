663335d746098cf11d36e0764b56dad747ea5e37
out-0 <ayoubarid000@gmail.com>
2026-01-08 19:44:36 +0100

    .

--- Module05/ex0/stream_processor.py
+++ Module05/ex0/stream_processor.py
@@ -1,2 +1,20 @@
 class DataProcessor():
     """Base class"""
+
+
+class NumericProcessor():
+    def process(self, data: Any) -> str
+    def validate(self, data: Any) -> bool
+    def format_output(self, result: str) -> str
+
+class TextProcessor():
+    def process(self, data: Any) -> str
+    def validate(self, data: Any) -> boo
+    def format_output(self, result: str) -> str
+
+class LogProcessor():
+    def process(self, data: Any) -> str
+    def validate(self, data: Any) -> boo
+    def format_output(self, result: str) -> str
+
+
